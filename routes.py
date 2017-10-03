3c9bdfc36e1009931469d9b653e85fe4b1eb3cb5
dagolinuxoid <daniel1kdago@gmail.com>
2017-10-03 23:57:28 +0300

    return to old slite3 - screw it :

--- routes.py
+++ routes.py
@@ -6,8 +6,7 @@ import psycopg2
 app = Flask(__name__)
 app.config.from_object('dev_conf')
 
-#db=SQL('sqlite:///{}'.format(app.config['DATABASE']))
-db=SQL('postgresql://{}'.format(app.config['DATABASE']))
+db=SQL('sqlite:///{}'.format(app.config['DATABASE']))
 # psst not only session but flash also reqieres setting up of a secret key
 # there is no route to template.html
 # template.html is using as a base
