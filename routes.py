baf7605981c252b89a5ed1946dd1632b3c3d1a03
dagolinuxoid <daniel1kdago@gmail.com>
2017-10-03 23:59:19 +0300

    get rid of pg

--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from functools import wraps
 from cs50 import SQL
-import psycopg2
 
 app = Flask(__name__)
 app.config.from_object('dev_conf')
